6fa6dc381bfee2f96e04e2cce1d040166de58e63
hongminpark <zhdals1026@gmail.com>
2024-11-25 19:13:12 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,18 +16,21 @@ let package = Package(
         .package(url: "https://github.com/microsoft/onnxruntime-swift-package-manager", revision: "ce64739"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "SwiftSegmentAnything", dependencies: [
-            .productItem(name: "onnxruntime", package: "onnxruntime-swift-package-manager", moduleAliases: nil, condition: nil)
-        ], resources: [
-            .process("Resources"),
-        ]),
+        .target(
+            name: "SwiftSegmentAnything",
+            dependencies: [
+                .product(name: "onnxruntime", package: "onnxruntime-swift-package-manager")
+            ],
+            resources: [
+                .process("Resources"),
+                // Add Info.plist for onnxruntime
+                .process("OnnxInfo.plist")
+            ]),
         .testTarget(
             name: "SwiftSegmentAnythingTests",
             dependencies: ["SwiftSegmentAnything"],
-        resources: [
-            .process("Resources")
-        ]),
+            resources: [
+                .process("Resources")
+            ]),
     ]
 )
